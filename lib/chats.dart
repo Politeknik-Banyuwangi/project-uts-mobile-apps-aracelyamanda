3290778ae5fbc2c8a705fb739214abb431435935
FlutLab.io <>
2022-05-31 07:25:34 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -18,6 +18,7 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            CardChats('Ridho', '19.00', 'Semangat buat hari ini', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
           ],
         ),
       ),
