4a549e9635cfd6fe2c8574a874e9179fced2382a
FlutLab.io <>
2022-05-31 07:28:19 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'widget/CardChats.dart';
+import 'Widget/CardChats.dart';
 
 class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
